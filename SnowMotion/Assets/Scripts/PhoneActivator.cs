188485ae286d91d63bf5724c5044127fe7af930a
mathildehaugum <mathilde_haugum@hotmail.com>
2021-03-26 13:16:07 +0100

    Add phone with weather display

---
+++ SnowMotion/Assets/Scripts/PhoneActivator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneActivator : MonoBehaviour
+{
+    public GameObject phoneObject;
+    // public bool isPhoneShown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+      if (Input.GetKeyDown(KeyCode.M)) {
+            MeshRenderer phone = phoneObject.GetComponent<MeshRenderer> ();
+            phone.enabled = !phone.enabled;
+        }   
+    }
+}
